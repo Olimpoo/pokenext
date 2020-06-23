89aa62d629d8544ed525f1850ca75977de63ae5e
jupitense <jupiter@thefutures.io>
2020-06-23 18:58:28 -0400

    Update pagination

--- pages/api/pokedex/index.js
+++ pages/api/pokedex/index.js
@@ -1,8 +1,13 @@
 import Pokedex from './config';
 
 export default async (req, res) => {
+  const interval = {
+    limit: 20,
+    offset: 0,
+  };
+
   try {
-    const pokemonList = await Pokedex.getPokemonsList();
+    const pokemonList = await Pokedex.getPokemonsList(interval);
     pokemonList.results = pokemonList.results.map((p, i) => ({ ...p, sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png` }));
     res.status(200).json({
       success: true,
