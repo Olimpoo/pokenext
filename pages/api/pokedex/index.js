eabe541642a7135a758a1ac50029021d7a4a5b16
jupitense <jupiter@thefutures.io>
2020-06-24 06:50:46 -0400

    add custom next/previous pagination

--- pages/api/pokedex/index.js
+++ pages/api/pokedex/index.js
@@ -3,12 +3,16 @@ import Pokedex from './config';
 export default async (req, res) => {
   const interval = {
     limit: req.query.limit || 20,
-    offset: 0,
+    offset: req.query.offset || 0,
   };
 
   try {
     const pokemonList = await Pokedex.getPokemonsList(interval);
+
+    pokemonList.next = `api/pokedex?offset=${parseFloat(interval.offset) + parseFloat(interval.limit)}&limit=${interval.limit}`;
+    pokemonList.previous = (parseFloat(interval.offset) - parseFloat(interval.limit) >= 0) ? `api/pokedex?offset=${parseFloat(interval.offset) - parseFloat(interval.limit)}&limit=${interval.limit}` : null;
     pokemonList.results = pokemonList.results.map((p, i) => ({ ...p, sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png` }));
+
     res.status(200).json({
       success: true,
       message: 'Pokémon listed!',
