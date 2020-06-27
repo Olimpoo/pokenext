336de07c901a38944f3f3abdda36c4a7c77e6ffb
jupitense <jupiter@thefutures.io>
2020-06-26 20:09:55 -0400

    add color types

--- pages/api/pokedex/types.js
+++ pages/api/pokedex/types.js
@@ -1,8 +1,36 @@
 import Pokedex from './config';
 
 export default async (req, res) => {
+  const colors = [
+    '#575738',
+    '#A12721',
+    '#5628E2',
+    '#833483',
+    '#695316',
+    '#61551F',
+    '#51580E',
+    '#705898',
+    '#565681',
+    '#AF510D',
+    '#1C59E9',
+    '#437C27',
+    '#896F06',
+    '#DC0949',
+    '#2D7B7B',
+    '#7038F8',
+    '#705848',
+    '#D23747',
+  ];
+
   try {
     const types = await Pokedex.getTypesList();
+    types.results = await types.results.map((type, i) => ({
+      ...type,
+      color: colors[i],
+    }));
+
+    await types.results.pop();
+    await types.results.pop();
 
     res.status(200).json({
       success: true,
